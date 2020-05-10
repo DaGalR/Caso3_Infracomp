4ff547979cdb4c908eea5710ce74c7a263fde6f0
Daniel Galndo <d.galindo@uniandes.edu.co>
2020-05-09 21:29:25 -0500

    Generator pro

--- src/generator/Generator.java
+++ src/generator/Generator.java
@@ -9,14 +9,27 @@ public class Generator {
 	
 	private static LoadGenerator generator;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Por favor escriba el número de tareas a ejecturar");
 		int numTasks = sc.nextInt();
 		System.out.println("Por favor escriba el tiempo entre tareas (ms)");
 		int tiempoEntreTareas = sc.nextInt();
-		Task tarea = new ClientServerTask();
+		System.out.println("Por favor indica que tipo de cliente quieres correr (1 seguridad, 0 sin seguridad)");
+		int tipoCliente = sc.nextInt();
+		Task tarea = null;
+
+		if(tipoCliente == 1) {
+			tarea = new ClientServerTask();
+
+		}
+		else if(tipoCliente == 0) {
+			tarea = new ClientServerNSTask(); 
+		}
+		else {
+			throw new Exception("No seleccionaste algo valido");
+		}
 		generator = new LoadGenerator("Client-Server load test", numTasks, tarea, tiempoEntreTareas);
 		generator.generate();
 		sc.close();
